994f55cec494009b3f8e21f0ebb8f4ecdb7d6f5e
Elona Kajtazi <MY_NAME@example.com>
2022-10-06 00:45:30 +0200

    Done!

--- src/main/java/com/zookeper/zookeper/ZooAnimalController.java
+++ src/main/java/com/zookeper/zookeper/ZooAnimalController.java
@@ -1,39 +1,66 @@
 package com.zookeper.zookeper;
 
-import java.util.ArrayList;
+import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ZooAnimalController {
+    @Autowired
+    private AnimalRepository animalRepository;
+
     @GetMapping("/animals")
-    public ArrayList<Animal> getAnimals() {
+    public List<Animal> getAnimals() {
+        return animalRepository.findAll();
 
-        ArrayList<Animal> animals = new ArrayList<>();
+    }
 
-        Animal animal = new Animal(1, "Tom", "Cat", true);
-        Animal animal1 = new Animal(2, "Jerry", "Mouse", true);
+    @PostMapping("/animals")
+    public Animal creaAnimal(@RequestBody Animal animalData) {
+        return animalRepository.save(animalData);
+    }
 
-        animals.add(animal);
-        animals.add(animal1);
-        return animals;
+    @DeleteMapping("/animals/{id}")
+    public String deleteAnimal(@PathVariable Integer id) {
+       animalRepository.deleteById(id);
+
+       return "Successfully deleted";
+    }
+
+    @PatchMapping("/animals/{id}")
+    public Animal updAnimal (@RequestBody Animal animalData, @PathVariable Integer id){
+        animalData.id = id;
+       return animalRepository.save(animalData);
 
     }
 }
 
+@Entity
 class Animal {
+    @Id
+    @GeneratedValue
     public Integer id;
     public String name;
     public String species;
     public Boolean isHungry;
 
-    public Animal(Integer id, String name, String species, boolean isHungry) {
-        this.id = id;
-        this.name = name;
-        this.species = species;
-        this.isHungry = isHungry;
+    public Animal() {
 
     }
 
 }
+
+interface AnimalRepository extends JpaRepository<Animal, Integer> {
+};
--- src/main/java/com/zookeper/zookeper/ZookeperApplication.java
+++ src/main/java/com/zookeper/zookeper/ZookeperApplication.java
@@ -2,7 +2,13 @@ package com.zookeper.zookeper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// Instructions
+// - Start a new Maven project ✅
+// - Make sure you have all dependencies in place - Spring web, H2, Hibernate. ✅
+// - Create a ZooAnimal class - id, name, species, origin, isHungry. ✅
+// - Seed your app with some data ✅
+// - Make sure your data is persisted when you restart your server. ✅
+// - Build a full CRUD for the animals ✅
 @SpringBootApplication
 public class ZookeperApplication {
 
